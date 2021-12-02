e04e952eb99e7dc42565e658c931cff33bf48b07
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2021-12-02 09:41:26 -0700

    02-2

--- 2021/02/prog.rs
+++ 2021/02/prog.rs
@@ -3,21 +3,23 @@ use std::fs;
 fn main () {
     let mut h: u32 = 0;
     let mut d: u32 = 0;
+    let mut aim: u32 = 0;
 
     let dat = fs::read_to_string("./in1.txt").unwrap();
     // let commands = dat.split("\n");
 
     for c in dat.lines() {
         if c.starts_with("forward") {
-            h += c.chars().last().unwrap().to_digit(10).unwrap()
+            h += c.chars().last().unwrap().to_digit(10).unwrap();
+            d += c.chars().last().unwrap().to_digit(10).unwrap() * aim
         }
         if c.starts_with("down") {
-            d += c.chars().last().unwrap().to_digit(10).unwrap()
+            aim += c.chars().last().unwrap().to_digit(10).unwrap();
         }
         if c.starts_with("up") {
-            d -= c.chars().last().unwrap().to_digit(10).unwrap()
+            aim -= c.chars().last().unwrap().to_digit(10).unwrap();
         }
     }
 
-    println!("h: {}, d: {}, multiplied: {}", h, d, h*d)
+    println!("h: {}, d: {}, multiplied: {}", h, d, h*d);
 }
